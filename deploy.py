48fd66c0132eea3e18931afdd8eb0f653d80623b
Sadeep <sadeepari@gmail.com>
2024-04-17 19:49:13 +0200

    add pkl

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.sav', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
